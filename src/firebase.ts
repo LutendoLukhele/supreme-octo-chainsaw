f6a2f4941009ac726fd2f3f3bf111d6b4680da81
lutendo <lutendolukheles@gmaill.com>
2025-05-10 18:11:27 +0900

    removed sensiste

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,17 @@
 // src/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { CONFIG } from './config'; // Import the centralized config
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: CONFIG.FIREBASE_API_KEY,
+    authDomain: CONFIG.FIREBASE_AUTH_DOMAIN,
+    databaseURL: CONFIG.FIREBASE_DATABASE_URL,
+    projectId: CONFIG.FIREBASE_PROJECT_ID,
+    storageBucket: CONFIG.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: CONFIG.FIREBASE_MESSAGING_SENDER_ID,
+    appId: CONFIG.FIREBASE_APP_ID,
+    measurementId: CONFIG.FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
